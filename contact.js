ff034988d44eb3a99cb43e6d0a56309cf9bf5d12
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-10-18 18:24:12 +0200

    feat: map resizing for desktop

---
+++ contact.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".submitDetails")
+console.log(btn)
+btn.addEventListener("click", (event)=> {
+    console.log("btn clicked")
+})
---
+++ dist/contact.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var btn = document.querySelector(".submitDetails");
+console.log(btn);
+btn.addEventListener("click", function (event) {
+  console.log("btn clicked");
+});
